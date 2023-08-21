24f9629ffdafc2cd5ec686ac65b81846c5f3cc0e
shubham.borkar <shubham.borkar@DT233>
2023-08-21 18:12:02 +0530

    Add project files.

---
+++ genric/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genric
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Vehicle Vehicle = new Vehicle()
+            {
+                Id = 1,
+                VehicleNoPlate = "MH34 DS 0001",
+                OwnerName = "shubham",
+                ContactNo = "8668582779"
+            };
+            
+            ParkingSpace ParkingSpace = new ParkingSpace()
+            {
+                NOOFSPACE = 400
+            };
+            ParkingTicket ParkingTicket = new ParkingTicket()
+            {
+                VehicleID = 1,
+                SpaceID = 300,
+                EntryDateTime = DateTime.Now,
+                ExitDateTime = DateTime.Now
+            };
+
+
+            TotalAmount TotalAmount = new TotalAmount()
+            {
+               TicketID = 500,
+               VehicleName = "audi",
+               Amount = 100,
+               ParkingDuration = 120
+
+            };
+
+        
+         Console.WriteLine(Vehicle.Id +" "+ Vehicle.VehicleNoPlate + " " +Vehicle.OwnerName + " " + Vehicle.ContactNo);
+         Console.WriteLine(ParkingSpace.NOOFSPACE);
+         Console.WriteLine(ParkingTicket.VehicleID + " " + ParkingTicket.SpaceID + " " + ParkingTicket.EntryDateTime + " " + ParkingTicket.ExitDateTime);
+         Console.WriteLine(TotalAmount.TicketID + " " + TotalAmount.VehicleName + " " + TotalAmount.Amount + " " + TotalAmount.ParkingDuration);
+         Console.ReadKey();
+        }
+
+        public class ticket<T, A>
+        {
+            public T Id { get; set; }
+            public A CreatedOn { get; set; }
+        }
+
+        public class Vehicle : ticket<int, DateTime>
+        {
+            public string VehicleNoPlate { get; set; }
+            public string OwnerName { get; set; }
+            public string ContactNo { get; set; }
+        }
+
+        public class ParkingSpace : ticket<int, DateTime>
+        {
+            public int NOOFSPACE { get; set; }
+        }
+
+        public class ParkingTicket : ticket<int, DateTime>
+        {
+            public int VehicleID { get; set; }
+            public int SpaceID { get; set; }
+            public DateTime EntryDateTime { get; set; }
+            public DateTime ExitDateTime { get; set; }
+        }
+
+        public class TotalAmount : ticket<int, DateTime>
+        {
+            public int TicketID { get; set; }
+            public string VehicleName { get; set; }
+            public int Amount { get; set; }
+            public int ParkingDuration { get; set; }
+        }
+     }  
+}
+
+
+
+
+
+
---
+++ genric2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genric2
+{
+    using System;
+
+    namespace GenericExample
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Vehicle<int, DateTime> vehicle = new Vehicle<int, DateTime>()
+                {
+                    Id = 1,
+                    VehicleNoPlate = "MH34 DS 0001",
+                    OwnerName = "shubham",
+                    ContactNo = "8668582779",
+                    CreatedOn = DateTime.Now
+                };
+                Console.WriteLine(vehicle.CreatedOn);
+
+                ParkingSpace<int, DateTime> parkingSpace = new ParkingSpace<int, DateTime>()
+                {
+                    NOOFSPACE = 400
+                };
+
+                ParkingTicket<int, DateTime> parkingTicket = new ParkingTicket<int, DateTime>()
+                {
+                    VehicleID = 1,
+                    SpaceID = 300,
+                    EntryDateTime = new DateTime(2023, 08, 12),
+                    ExitDateTime = new DateTime(2023, 08, 13)
+                };
+
+                TotalAmount<int, DateTime> totalAmount = new TotalAmount<int, DateTime>()
+                {
+                    TicketID = 500,
+                    VehicleName = "audi",
+                    Amount = "100",
+                    ParkingDuration = "120"
+                    
+                };
+            }
+
+            public class ticket<T, A>
+            {
+                public T Id { get; set; }
+                public A CreatedOn { get; set; }
+            }
+
+            public class Vehicle<T, A> : ticket<T, A>
+            {
+                public string VehicleNoPlate { get; set; }
+                public string OwnerName { get; set; }
+                public string ContactNo { get; set; }
+            }
+
+            public class ParkingSpace<T, A> : ticket<T, A>
+            {
+                public int NOOFSPACE { get; set; }
+            }
+
+            public class ParkingTicket<T, A> : ticket<T, A>
+            {
+                public int VehicleID { get; set; }
+                public int SpaceID { get; set; }
+                public A EntryDateTime { get; set; }
+                public A ExitDateTime { get; set; }
+            }
+
+            public class TotalAmount<T, A> : ticket<T, A>
+            {
+                public int TicketID { get; set; }
+                public string VehicleName { get; set; }
+                public string Amount { get; set; }
+                public string ParkingDuration { get; set; }
+                
+            }
+        }
+    }
+}
